8561205e6b2952ce157020ea38a3c48b41bcdad4
Gledson  de Carvalho Santos <gledsondecarvalhosantos@MacBook-Air-de-Gledson.local>
2020-12-09 16:29:45 -0300

    corrige erro na confirmação do alert

--- src/@fuse/components/alert/alert.component.ts
+++ src/@fuse/components/alert/alert.component.ts
@@ -1,5 +1,5 @@
 import { Component, Injectable, OnInit } from '@angular/core';
-import Swal, { SweetAlertIcon } from 'sweetalert2';
+import Swal, { SweetAlertIcon, SweetAlertResult } from 'sweetalert2';
 
 @Component({
   selector: 'app-alert',
@@ -24,21 +24,27 @@ export class AlertComponent implements OnInit {
     });
   }
 
-  confirmacao(title: string, text: string, confirmButtonText: string, textSuccess: string, header: string) {
-    return Swal.fire({
-      title: title,
-      text: text,
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: confirmButtonText,
-      heightAuto: false,
-      cancelButtonText: 'Cancelar'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this.show(header, textSuccess, 'success')
-      }
+  confirmacao(title: string, text: string, confirmButtonText: string, textSuccess: string, header: string): Promise<any> {
+    return new Promise<any> ((resolve, reject) => {
+      Swal.fire({
+        title: title,
+        text: text,
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: confirmButtonText,
+        heightAuto: false,
+        cancelButtonText: 'Cancelar'
+      }).then((result: SweetAlertResult) => {
+        if (result.isConfirmed) {
+          resolve();
+          this.show(header, textSuccess, 'success')
+        } else {
+          reject();
+        }
+      })
     })
+    
   }
 }
